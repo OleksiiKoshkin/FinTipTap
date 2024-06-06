0ae59ba91c02302d6f1a49c6a32d73c55d1f9735
Oleksii Koshkin <oleksii.koshkin@fintastic.as>
2024-06-06 19:22:58 +0200

    redirect

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,18 +1,23 @@
 import {useRouteError} from "react-router-dom";
 import {Alert, AlertTitle, Typography} from "@mui/material";
 import React from "react";
+import {MainMenu} from "./MainMenu.tsx";
 
 export const ErrorPage: React.FC = () => {
     const error: any = useRouteError();
     console.error(error);
 
     return (
-        <Alert variant="filled" severity="error" sx={{textAlign: 'left'}}>
-            <AlertTitle>Oops</AlertTitle>
-            <Typography>Sorry, an unexpected error has occurred.</Typography>
-            <div>
-                <i>{error.statusText || error.message}</i>
-            </div>
-        </Alert>
+        <>
+            <Alert variant="filled" severity="error" sx={{textAlign: 'left'}}>
+                <AlertTitle>Oops</AlertTitle>
+                <Typography>Sorry, an unexpected error has occurred.</Typography>
+                <div>
+                    <i>{error.statusText || error.message}</i>
+                </div>
+            </Alert>
+
+            <MainMenu/>
+        </>
     );
 }
